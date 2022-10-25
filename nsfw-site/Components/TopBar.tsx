fca628836a71c9170bc9eaf6ea8f4cb86ebbfaa7
NSFW <nftandromeda1988@gmail.com>
2022-10-26 01:36:34 +0200

    immagini di background

--- nsfw-site/Components/TopBar.tsx
+++ nsfw-site/Components/TopBar.tsx
@@ -2,14 +2,16 @@ import { FC } from "react";
 import styles from "../styles/Home.module.css";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import Image from "next/image"
-
+import Logo from "../image/LogoSmall.png"
 export const TopBar: FC = () => {
   return (
+   
     <div className={styles.topBar}>
+       <Image src={Logo} alt='logo' height={60} width={60}/>
       <div className={styles.topBarWrapper}>
-        <ul>
-          <li><a>Home</a></li>
-        </ul>  
+          <ul>
+            <li><a>Home</a></li>
+          </ul>
       </div>
       <WalletMultiButton/>
     </div>
