2bdf409139e2bc6db91e482c6016026c8a48586c
NSFW <nftandromeda1988@gmail.com>
2022-10-26 16:24:14 +0200

    Aggiornamento Topbar 1

--- nsfw-site/Components/TopBar.tsx
+++ nsfw-site/Components/TopBar.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import styles from "../styles/Home.module.css";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 import Image from "next/image"
+import Link from "next/link"
 import Logo from "../image/LogoSmall.png"
 export const TopBar: FC = () => {
   return (
@@ -10,8 +11,8 @@ export const TopBar: FC = () => {
       
       <div className={styles.topBarWrapper}>
           <ul>
-            <li><Image src={Logo} alt='logo' height={60} width={60}/></li>
-            <li><a>Home</a></li>
+            <li className={styles.topBarLogoLi}><Link href="/"><Image className={styles.topBarLogo} src={Logo} alt='logo' height={60} width={60}/> NSFW</Link> </li>
+            <li><Link href="/">Home</Link></li>
             <li><WalletMultiButton/></li>
           </ul>
            
